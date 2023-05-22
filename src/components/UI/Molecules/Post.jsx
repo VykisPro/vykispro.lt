a68eb4d1ded230426ce451c997856e20ec100c1e
VykisPro <ramanauskasviktoras@gmail.com>
2023-05-22 19:05:54 +0300

    User avatar styled

--- src/components/UI/Molecules/Post.jsx
+++ src/components/UI/Molecules/Post.jsx
@@ -6,8 +6,17 @@ const StyledPostDiv = styled.div`
 border:1px solid black;
 max-width:80%;
 margin:20px auto;
-padding:50px;
+padding:10px;
 `;
+const StyledUserInfoDiv = styled.div`
+display:flex;
+align-items:center;
+gap:20px;
+>img{
+    with:50px;
+    height:50px;
+    border-radius:50%;
+}`
 
 
 const Post = ({ data }) => {
@@ -17,11 +26,11 @@ const user = users.find(a => a.id === data.userId);
 
     return (
         <StyledPostDiv>
-            { users ?
-             <div>
+            { users.length ?
+             <StyledUserInfoDiv>
                 <img src={user.avatarURL} alt="Paskyros avataras" />
                 <p>{user.userName}</p>
-            </div>: <p> Vartotojas kraunamas </p>
+                </StyledUserInfoDiv>: <p> Vartotojas kraunamas </p>
             }
             <div>
                 <h3>{data.title}</h3>
